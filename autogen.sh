b7c16db5f5e6d3ebc5d3d60861abe50d4eb6195f
Fridrich Štrba <fridrich.strba@bluewin.ch>
2010-07-20 12:15:44 +0200

    Fix the autogen.sh to actually work

--- autogen.sh
+++ autogen.sh
@@ -4,14 +4,13 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="evolution-groupwise"
+PKG_NAME="evolution-ews"
 REQUIRED_AUTOCONF_VERSION=2.58
 REQUIRED_AUTOMAKE_VERSION=1.9
 REQUIRED_LIBTOOL_VERSION=2.2
 REQUIRED_INTLTOOL_VERSION=0.35.5
 
 (test -f $srcdir/configure.ac \
-  && test -f $srcdir/ChangeLog \
   && test -d $srcdir/src/camel) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the" >&2
     echo " top-level $PKG_NAME directory" >&2
