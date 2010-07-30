d4cb4a458ce694495ae489b36f40bb2edb653e2d
Johnny Jacob <johnnyjacob@gmail.com>
2010-07-30 16:39:15 +0530

    Introducing unit tests for libews.

---
+++ src/server/tests/test_connection.c
@@ -0,0 +1,14 @@
+/*Place holder for connection test cases*/
+#include <stdio.h>
+#include <glib.h>
+#include <glib/gprintf.h>
+
+void connection_tests_run ()
+{
+	g_printf ("Testing Connection\n");
+}
+
+void autodiscovery_tests_run ()
+{
+	g_printf ("Testing Autodiscovery\n");
+}
