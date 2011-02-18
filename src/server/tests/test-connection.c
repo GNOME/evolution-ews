8a9f76f7f629e77e59e07c52c69f47222915e438
Johnny Jacob <johnnyjacob@gmail.com>
2011-02-18 16:44:17 +0530

    Tests: Added license string

--- src/server/tests/test-connection.c
+++ src/server/tests/test-connection.c
@@ -1,4 +1,26 @@
-/*Place holder for connection test cases*/
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Authors : Johnny Jacob <johnnyjacob@gmail.com>
+ *
+ * Copyright (C) 1999-2011 Novell, Inc. (www.novell.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
+/* APIs : e_ews_autodiscover_ws_url, e_ews_connection_new */
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
