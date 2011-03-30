2ec8567b50436bbd3271639fe502cf584b7ef0b6
Mandy Wu <Mandy.Wu@intel.com>
2011-03-30 21:52:59 +0800

    Add test case for get_attachments

---
+++ src/server/tests/test-getattachment.c
@@ -0,0 +1,127 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Authors : Mandy Wu <mandy.wu@intel.com>
+ *
+ * Copyright © 2011 Intel Corporation. (www.intel.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
+/* APIs : e_ews_connection_get_attachments */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <glib.h>
+#include <glib/gprintf.h>
+#include "utils.h"
+#include <e-ews-connection.h>
+#include <e-ews-item.h>
+#include <e-ews-message.h>
+
+
+static void op_test_get_attachments ();
+void get_attachments_tests_run ();
+
+static GMainLoop *main_loop;
+
+static void
+get_attachments_ready_callback (GObject *object, GAsyncResult *res, gpointer user_data)
+{
+	EEwsConnection *cnc = E_EWS_CONNECTION (object);
+	GError *error = NULL;
+	GSList *ids = NULL, *l;
+
+	e_ews_connection_get_attachments_finish	(cnc, res, &ids, &error);
+
+	if (error != NULL) {
+		g_print ("Unable to get attachment: %s :%d \n", error->message, error->code);
+		goto quit;
+	}
+
+	for (l = ids; l != NULL; l = g_slist_next (l)) {
+		CalendarAttachment *att = (CalendarAttachment *)l->data;
+		g_print ("Got Attachment: \nType is %s \n", att->type);
+		if (!g_ascii_strcasecmp (att->type, "ItemAttachment")) {
+			EEwsItem *item = (EEwsItem *) att->data;
+			if (item) {
+				g_print ("Subject %s\n", e_ews_item_get_subject(item));
+				g_print ("MimeContent %s\n", e_ews_item_get_mime_content(item));
+			}
+		}
+		else if (att->type == "FileAttachment") {
+
+		}
+		break;
+	}
+
+quit:
+	g_main_loop_quit(main_loop);
+}
+
+static void
+op_test_get_attachments()
+{
+	const gchar *username;
+	const gchar *password;
+	const gchar *uri;
+	const gchar *attachmentid;
+	EEwsConnection *cnc;
+	GCancellable *cancellable;
+	GSList *ids = NULL;
+
+	cancellable = g_cancellable_new ();
+
+	util_get_login_info_from_env (&username, &password, &uri);
+	g_assert_cmpstr (username, !=, NULL);
+	g_assert_cmpstr (password, !=, NULL);
+	g_assert_cmpstr (uri, !=, NULL);
+
+	cnc = e_ews_connection_new (uri, username, password, NULL);
+	g_assert (cnc != NULL);
+
+	//attachmentid = "AAASAG1hbmR5Lnd1QGludGVsLmNvbQBGAAAAAACdSXexmsgJTpd3WpdX6ulXBwAm9E+BClHfQqEnvCoGvhheAAAAjpb6AADy6etzSLgmT4a2q4FRzi15ABEAn2nlAAABEgAQANZ8v7RLDR9Pnd9QRxPK0c0=";
+	attachmentid = "AAASAG1hbmR5Lnd1QGludGVsLmNvbQBGAAAAAACdSXexmsgJTpd3WpdX6ulXBwAm9E+BClHfQqEnvCoGvhheAAAAjpb6AACIeDU1D80fTrC3245yXdhOADUAPRACAAABEgAQAPyRSkVm3h1Dt6ZAKCTrk1s=";
+	ids = g_slist_append (ids, (gpointer *)attachmentid);
+
+	e_ews_connection_get_attachments_start	(cnc, EWS_PRIORITY_MEDIUM,
+						 ids, TRUE,
+						 get_attachments_ready_callback,
+						 NULL, NULL,
+						 cancellable, NULL);
+}
+
+static gboolean
+idle_cb (gpointer data)
+{
+	g_printf ("Testing get attachments..... \n");
+	op_test_get_attachments ();
+
+	return FALSE;
+}
+
+/*Run tests*/
+void get_attachments_tests_run ()
+{
+	g_type_init ();
+	g_thread_init (NULL);
+
+	main_loop = g_main_loop_new (NULL, TRUE);
+	g_idle_add ((GSourceFunc) idle_cb, NULL);
+	g_main_loop_run (main_loop);
+
+	/* terminate */
+	g_main_loop_unref (main_loop);
+}
--- src/server/tests/test-libews.c
+++ src/server/tests/test-libews.c
@@ -14,6 +14,7 @@ void cuditem_tests_run ();
 void autocompletion_tests_run ();
 void createfolder_tests_run ();
 void deletefolder_tests_run (gconstpointer data);
+void get_attachments_tests_run ();
 
 static void
 finalize_test_data ()
@@ -42,6 +43,8 @@ int main (int argc, char *argv[])
 	g_test_add_data_func ("/libews/deletefolder", &folder_id, deletefolder_tests_run);
 	
 	g_test_add_func ("/libews/cuditem", cuditem_tests_run);
+
+	g_test_add_func ("/libews/getattachment", get_attachments_tests_run);
 
 	ret = g_test_run ();
 	finalize_test_data ();
