054a59faea96b504cb32cb2e2b357beb7d1f8981
Chenthill Palanisamy <pchenthill@novell.com>
2011-04-01 14:34:05 +0530

    Add e-sqlite3-vfs.[ch]. This should be later put in libedataserver in eds. Camel and libedata-book libraries should be sharing it.

---
+++ src/utils/e-sqlite3-vfs.h
@@ -0,0 +1,26 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ *
+ * Copyright (C) 1999-2011 Novell, Inc. (www.novell.com)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ * USA
+ */
+
+#ifndef E_SQLITE3_VFS_H
+#define E_SQLITE3_VFS_H
+
+void e_sqlite3_vfs_init (void);
+
+#endif
