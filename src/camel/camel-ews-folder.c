8d4d1b3330637476de454a494826083ad2d7135a
Pavel Ocheretny <pocheretny@src.gnome.org>
2011-06-26 13:15:54 +0300

    Store the Meeting request in to calendar_item_accept_id and pass to modify the mime content block

--- src/camel/camel-ews-folder.c
+++ src/camel/camel-ews-folder.c
@@ -386,7 +386,7 @@ camel_ews_folder_get_message (CamelFolder *folder, const gchar *uid, gint pri, G
 		e_ews_item_get_item_type (items->data) == E_EWS_ITEM_TYPE_MEETING_MESSAGE ||
 		e_ews_item_get_item_type (items->data) == E_EWS_ITEM_TYPE_MEETING_RESPONSE) {
 		GSList *items_req = NULL;
-		const EwsId *associated_calendar_id;
+		const EwsId *calendar_item_accept_id;
 
 		// Get AssociatedCalendarItemId with second get_items call
 		res = e_ews_connection_get_items (cnc, pri, ids, "IdOnly", "meeting:AssociatedCalendarItemId",
@@ -402,13 +402,12 @@ camel_ews_folder_get_message (CamelFolder *folder, const gchar *uid, gint pri, G
 			}
 			goto exit;
 		}
-		associated_calendar_id = e_ews_item_get_calendar_item_accept_id (items_req->data);
+		calendar_item_accept_id = e_ews_item_get_calendar_item_accept_id (items_req->data);
 		/*In case of non-exchange based meetings invites the calendar backend have to create the meeting*/
-		if (associated_calendar_id) {
-			mime_fname_new = ews_update_mgtrequest_mime_calendar_itemid (mime_content,
-										     associated_calendar_id,
-										     error);
+		if (!calendar_item_accept_id) {
+			calendar_item_accept_id = e_ews_item_get_id (items->data);
 		}
+		mime_fname_new = ews_update_mgtrequest_mime_calendar_itemid (mime_content, calendar_item_accept_id, error);
 		if (mime_fname_new)
 			mime_content = (const gchar *) mime_fname_new;
 
