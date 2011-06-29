dafaa585311c4514613e56c2dba439c2907b109c
Chenthill Palanisamy <pchenthill@novell.com>
2011-06-29 17:13:34 +0530

    Set read mode also while creating oal cache file

--- src/server/e-ews-connection.c
+++ src/server/e-ews-connection.c
@@ -1601,7 +1601,7 @@ ews_soup_got_chunk (SoupMessage *msg, SoupBuffer *chunk, gpointer user_data)
 		data->progress_fn (data->progress_data, pc);
 	}
 
-	fd = g_open (data->cache_filename, O_WRONLY | O_CREAT);
+	fd = g_open (data->cache_filename, O_RDONLY | O_WRONLY | O_CREAT);
 	if (fd != -1) {
 		if (write (fd, (const gchar*)chunk->data, chunk->length) != chunk->length) {
 			g_set_error (&data->error, EWS_CONNECTION_ERROR, EWS_CONNECTION_ERROR_UNKNOWN,
