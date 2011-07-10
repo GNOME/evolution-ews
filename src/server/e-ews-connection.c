e35f83c2e7d05e4e3bad9e47319bf44486df326f
Chenthill Palanisamy <pchenthill@novell.com>
2011-07-10 16:20:39 +0530

    Open the file in append mode

--- src/server/e-ews-connection.c
+++ src/server/e-ews-connection.c
@@ -1617,7 +1617,7 @@ ews_soup_got_chunk (SoupMessage *msg, SoupBuffer *chunk, gpointer user_data)
 		data->progress_fn (data->progress_data, pc);
 	}
 
-	fd = g_open (data->cache_filename, O_RDONLY | O_WRONLY | O_CREAT, 0600);
+	fd = g_open (data->cache_filename, O_RDONLY | O_WRONLY | O_APPEND | O_CREAT, 0600);
 	if (fd != -1) {
 		if (write (fd, (const gchar*)chunk->data, chunk->length) != chunk->length) {
 			g_set_error (&data->error, EWS_CONNECTION_ERROR, EWS_CONNECTION_ERROR_UNKNOWN,
