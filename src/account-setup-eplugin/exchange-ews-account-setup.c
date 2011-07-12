b3eccde6271d2c6ebbba90e9b8466daffa012746
Chenthill Palanisamy <pchenthill@novell.com>
2011-07-12 13:50:08 +0530

    Replace gtk_combo_box_new to gtk_combo_box_new_text so that it would work in against evolution 2.32.

--- src/account-setup-eplugin/exchange-ews-account-setup.c
+++ src/account-setup-eplugin/exchange-ews-account-setup.c
@@ -48,7 +48,7 @@
 #define GTK_COMBO_BOX_TEXT GTK_COMBO_BOX
 #define GTK_IS_COMBO_BOX_TEXT GTK_IS_COMBO_BOX
 #define gtk_combo_box_text_append_text gtk_combo_box_append_text
-#define gtk_combo_box_text_new gtk_combo_box_new
+#define gtk_combo_box_text_new gtk_combo_box_new_text
 #define GtkComboBoxText GtkComboBox
 #endif
 
