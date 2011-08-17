78f5f81e7485ccd63a221d5e29ec0f2783b3ca9a
David Woodhouse <David.Woodhouse@intel.com>
2011-08-17 15:45:01 +0100

    Bug 656482: Fix leak of old mi->change_key in sync_updated_items()

--- src/camel/camel-ews-utils.c
+++ src/camel/camel-ews-utils.c
@@ -919,6 +919,7 @@ camel_ews_utils_sync_updated_items (CamelEwsFolder *ews_folder, GSList *items_up
 								 server_flags, NULL))
 				camel_folder_change_info_change_uid (ci, mi->info.uid);
 
+			g_free (mi->change_key);
 			mi->change_key = g_strdup (id->change_key);
 			mi->info.dirty = TRUE;
 
