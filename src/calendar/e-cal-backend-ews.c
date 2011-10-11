d2d4154ec71ec38c27bd112794f03c1957972ccb
Chenthill Palanisamy <pchenthill@novell.com>
2011-10-11 15:04:18 +0530

    Fix the appointment to be deleted properly

--- src/calendar/e-cal-backend-ews.c
+++ src/calendar/e-cal-backend-ews.c
@@ -1074,6 +1074,9 @@ e_cal_backend_ews_remove_object (ECalBackend *backend, EDataCal *cal,
 	PRIV_LOCK (priv);
 
 	comp = e_cal_backend_store_get_component (priv->store, uid, rid);
+	
+	if (!rid || !*rid)
+		rid = NULL;
 
 	if (rid) {
 		parent = e_cal_backend_store_get_component (priv->store, uid, NULL);
